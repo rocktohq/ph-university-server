6e16246c958cc19c7aa03fa4aabdc19d7acda008
rocktohq <itzmonir@gmail.com>
2024-12-05 23:18:37 +0600

    StudentID Generation Bug Fixed

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -2,21 +2,47 @@
 import { TAcademicSemester } from "../academicSemester/academicSemester.interface";
 import { User } from "./user.model";
 
-// Find last student
-const findLastStudent = async () => {
-  const lastStudent = await User.findOne({ role: "student" }, { _id: 0, id: 1 })
-    .sort({ createdAt: -1 })
+const findLastStudentId = async () => {
+  const lastStudent = await User.findOne(
+    {
+      role: "student",
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
     .lean();
 
-  return lastStudent ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
-// Student ID generation
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudent()) || (0).toString();
-  let increment = (Number(currentId) + 1).toString().padStart(4, "0");
+  // For the first time ID will be 0000
+  // For the second or later time ID will be incremented by 1
+  let currentId = (0).toString(); // 0000: Deafult digits
 
-  increment = `${payload.year}${payload.code}${increment}`;
+  const lastStudentId = await findLastStudentId();
+  // 2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); // 01: Semester code
+  const lastStudentYear = lastStudentId?.substring(0, 4); // 2030: Semester year
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
 
-  return increment;
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6); // 0001: Last four digits
+  }
+
+  // Increment the last ID by 1 and pad with leading zeros to make it 4 digits long
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
+  incrementId = `${payload.year}${payload.code}${incrementId}`;
+
+  return incrementId;
 };
